907b17467b3d17442e79c053c5cd468b31ae4f85
Allan Araújo <allan.hal@gmail.com>
2025-09-27 00:20:37 -0300

    adding backend with gemini-ai

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+import express from "express";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const app = express();
+const port = process.env.PORT || 8080;
+import "dotenv/config.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send(`Hello World!`);
+});
+
+app.get("/ia", (req, res) => {
+  const API_KEY = process.env.GEMINI_API_KEY || "";
+  console.log(API_KEY);
+  // https://github.com/google-gemini/generative-ai-js
+  const genAI = new GoogleGenerativeAI(API_KEY || "");
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-flash",
+  });
+  const prompt = "Qual o presidente do mundo?";
+  model
+    .generateContent(prompt)
+    .then((result) => {
+      res.send(result.response.text());
+    })
+    .catch((error) => {
+      res.send({ message: "error", error });
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
